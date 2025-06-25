ab540f21f284f2afcd9c63f069733841145c9333
Alexis Contente <a.contente@quant-cube.com>
2025-06-25 17:37:04 +0200

    chore(footer): create footer and add component to page

---
+++ app/components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-500 text-white text-center p-4">
+      © 2024 - Développé par Alexis Contente - Application de réservation de
+      place de bureau
+    </footer>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Header />
+      <main className="flex-1 p-6">{/* Contenu principal */}</main>
+      <Footer />
     </div>
   );
 }
