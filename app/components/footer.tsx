55994800f730ae7c64175e629f5e69dc925d5fc8
Alexis Contente <a.contente@quant-cube.com>
2025-07-02 11:43:04 +0200

    chore(footer): update copyright year to 2025

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-blue-500 text-white text-center p-4">
-      © 2024 - Developed by Alexis Contente - Workplace booking application
+      © 2025 - Developed by Alexis Contente - Workplace booking application
     </footer>
   );
 }
