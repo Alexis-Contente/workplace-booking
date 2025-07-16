e91eb096f36501b9d1467f62aa0436a0d2f12b05
Alexis Contente <a.contente@quant-cube.com>
2025-07-16 09:51:35 +0200

    chore(toast): reduce the duration of toasts and disable the close button for a better user experience

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -9,8 +9,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
       className="toaster group"
       position="top-right"
       visibleToasts={6}
-      duration={6000}
-      closeButton={true}
+      duration={3500}
+      closeButton={false}
       richColors={true}
       expand={true}
       offset="20px"
@@ -28,7 +28,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
             "0 10px 40px -10px rgba(0, 0, 0, 0.25), 0 0 0 1px rgba(255, 255, 255, 0.1), inset 0 1px 0 rgba(255, 255, 255, 0.1)",
         },
         className: "my-toast",
-        duration: 6000,
+        duration: 3500,
       }}
       style={
         {
